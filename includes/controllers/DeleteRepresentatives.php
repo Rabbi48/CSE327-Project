9ab003d3eaaaca840aa366e8bc24d07b795406cc
imran17129 <96998841+imran17129@users.noreply.github.com>
2022-01-05 02:00:10 +0600

    Create DeleteRepresentatives.php

---
+++ includes/controllers/DeleteRepresentatives.php
@@ -0,0 +1,16 @@
+<?php 
+    /**
+    * @brief This class loads the warning page .
+    */
+class DeleteRepresentatives extends Controller{
+        /**
+    * @brief Loads all the data after View is executed
+    * @param no parameters
+    * @retval result - Data gets loaded on or from the page.
+    */
+    public static function run(){
+
+        
+        }
+
+    }
