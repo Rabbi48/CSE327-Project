2ec33aa6eca1f9acc8c25aa665be36961528306c
Rabbi48 <94570628+Rabbi48@users.noreply.github.com>
2022-01-05 02:17:17 +0600

    Update Cart.php

--- includes/controllers/Cart.php
+++ includes/controllers/Cart.php
@@ -1,1 +1,55 @@
+<?php
 
+/**
+ * @brief Cart class. Retrieve and show all the data for cart and print it to the views and it extebds to Controller class.
+ */
+class Cart extends Controller
+{
+    /**
+     * @brief A cart that showcase the product list and price added by a user.
+     * @param  No parameter.
+     * @return Prints the cart of the current user.
+     */
+    public static function cartP()
+    {
+        $getEid = new CrudModel();
+        $eid = $getEid->getEid();
+
+        $result = self::query("SELECT * FROM cart where emp_id = '$eid'");
+        /**< result - Result retrived from database */
+        $total = 0;
+        /**< total - Total price of the Product added by the user, initially start with 0 */
+        foreach ($result as $item) {
+            $total = $total + $item[5];
+?>
+            <tr>
+                <td><?php echo $item[1]; ?></td>
+                <td>
+                    <form action="removeanitem?eid=<?php echo $eid; ?> &mid=<?php echo $item[0]; ?>" method="POST"><button>❌</button></form>
+                </td>
+                <td><?php echo $item[4]; ?></td>
+                <td><?php echo $item[2]; ?></td>
+                <td><?php echo $item[5]; ?></td>
+            </tr>
+        <?php
+        }
+        ?>
+        <tr>
+            <td colspan=3></td>
+            <td>Total Price</td>
+            <td><?php echo $total; ?></td>
+        </tr>
+        <tr>
+            <td colspan=3></td>
+            <td>
+                <form action="clearcart?eid=<?php echo $eid ?>" method="POST"><button class="button button1">Clear All</button></form>
+            </td>
+            <td>
+                <form action="checkout?eid=<?php echo $eid ?>" method="POST"><button class="button button2">Checkout</button></form>
+            </td>
+        </tr>
+
+<?php
+    }
+}
+?>
