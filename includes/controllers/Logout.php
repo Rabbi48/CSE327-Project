1c2f936fb49dcc84e10f2e9ab4d55cc5f71463c9
Rabbi48 <94570628+Rabbi48@users.noreply.github.com>
2022-01-05 02:43:43 +0600

    Update Logout.php

--- includes/controllers/Logout.php
+++ includes/controllers/Logout.php
@@ -1,1 +1,18 @@
+<?php
+    /**
+    * @brief This class loads all relevent data related to home page.
+    */
+class Logout extends Controller {
+       /**
+    * @brief Loads all the data after View is executed
+    * @param no parameters
+    * @retval result - Data gets loaded on or from the page.
+    */
+    public static function run(){
+        $deleteEid = new CrudModel();
+        $deleteEid->deleteEid();
+        header('location: Index.php');
+    }
+
+}
 
