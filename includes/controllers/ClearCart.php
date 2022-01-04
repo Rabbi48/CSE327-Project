72fc0a305c554c6e87638cc1bbb0deeb5d6d1746
Rabbi48 <94570628+Rabbi48@users.noreply.github.com>
2022-01-05 02:21:25 +0600

    Update ClearCart.php

--- includes/controllers/ClearCart.php
+++ includes/controllers/ClearCart.php
@@ -1,1 +1,19 @@
+<?php 
+    /**
+    * @brief ClearCart class. Do stuffs to clear the cart and it extebds to Controller class.
+    */
+class ClearCart extends Controller{
+    /**
+    * @brief Clear the car at a single click..
+    * @param No, parameter. 
+    * @return Clears the whole cart at a time. 
+    */
+    public static function clearAll(){
+        $getEid = new CrudModel();
+        $eid = $getEid->getEid();
+        $model = new AddToCartModel(); /**< model - An object of AddToCartModel Class */
+        $model->clearCart($eid); /**< result - Result retrived from database */
+        header('Location: cart');
+    }
+    }
 
